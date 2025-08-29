4364e2dadf1ebb897b0f174858c29e833a472dfd
namnguyen2k1 <nanam133hg@gmail.com>
2025-08-29 15:28:32 +0700

    feat: create device-service application

---
+++ apps/device-service/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { DeviceServiceModule } from './device-service.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(DeviceServiceModule);
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
