f170b6a6db2099a0a631bd4b5d45d4e8a2f55435
namnguyen2k1 <nanam133hg@gmail.com>
2025-08-30 00:54:34 +0700

    refactor: delete assistan-service, file-service, update package.json

--- apps/assistant-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { AssistantServiceModule } from "./assistant-service.module";
-
-async function bootstrap() {
-  const app = await NestFactory.create(AssistantServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/file-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { FileServiceModule } from "./file-service.module";
-
-async function bootstrap() {
-  const app = await NestFactory.create(FileServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
